5d1d22aa662c7b182a86a2a022fd71e2966a7bac
Victor A. Martinez <martinezsantiagovictor@outlook.com>
2017-02-12 21:25:26 -0400

    updated version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='geteventstore',
-    version='0.0.1',
+    version='0.0.2',
     description='Python Implementation of the EventStore API client',
     long_description=readme,
     author='Victor A Martinez',
