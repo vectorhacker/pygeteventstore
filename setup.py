68393d1a915dcf490b1a36e81b668fb79eeab1e8
Victor A. Martinez <martinezsantiagovictor@outlook.com>
2017-02-12 21:42:44 -0400

    maybe fixed

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('LICENSE') as f:
 
 setup(
     name='geteventstore',
-    version='0.0.2',
+    version='0.0.3',
     description='Python Implementation of the EventStore API client',
     long_description=readme,
     author='Victor A Martinez',
