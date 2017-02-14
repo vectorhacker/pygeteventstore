ad2fc8c0aac9f3cf9139c4f422e71d178e45c3be
Victor A. Martinez <martinezsantiagovictor@outlook.com>
2017-02-14 11:42:15 -0400

    updated version and license

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ with open('LICENSE') as f:
 
 setup(
     name='geteventstore',
-    version='0.1.16',
+    version='0.0.1',
     description='Python HTTP Client for the Event Store API',
     long_description=readme,
-    author='Victor A Martinez',
+    author='Victor A. Martinez Santiago',
     author_email='victor@plazsa.com',
-    url='https://gitlab.plazsa.com/vectorhacker/pygeteventstore',
+    url='https://github.com/vectorhacker/pygeteventstore',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
